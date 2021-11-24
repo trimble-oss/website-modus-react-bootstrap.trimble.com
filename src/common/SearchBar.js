bcd7ad93d92fff6f8f653db54dc4c61bd6800983
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2021-11-24 21:05:38 +0530

    improve search bar

--- src/common/SearchBar.js
+++ src/common/SearchBar.js
@@ -1,7 +1,9 @@
-import React, { useState, useRef, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 import {
+  Dropdown,
   Form,
   FormControl,
+  ListGroup,
   Nav,
   Overlay,
 } from "@trimbleinc/modus-react-bootstrap"
@@ -13,12 +15,20 @@ const SearchBar = ({ location }) => {
 
   const [show, setShow] = useState(false)
   const [target, setTarget] = useState(null)
+
   const ref = useRef(null)
+  const dropdownRef = useRef(null)
+
+  const handleClickOutside = e => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target))
+      setShow(false)
+  }
+
+  document.addEventListener("mousedown", handleClickOutside)
 
   const handleChange = e => {
     let userInput = e.target.value
     setSearchQuery(userInput)
-
     if (userInput && userInput.length > 2 && window.__LUNR__) {
       window.__LUNR__.__loaded.then(lunr => {
         const refs = lunr.en.index.search(`*${userInput}*`)
@@ -38,63 +48,65 @@ const SearchBar = ({ location }) => {
   }
 
   const handleKeyDown = e => {
+    // if (e.keyCode === 38 || e.keyCode === 40) dropdownRef.current.focus()
     // arrow up/down button should select next/previous list element
-    if (e.keyCode === 38 && cursor > 0) {
-      setCursor(prevState => prevState - 1)
-    } else if (e.keyCode === 40 && cursor < results.length - 1) {
-      setCursor(prevState => prevState + 1)
-    } else if (e.keyCode === 13 && cursor > -1 && results[cursor].itemRef) {
-      results[cursor].itemRef.focus()
-      results[cursor].itemRef.click()
-    }
+    // if (e.keyCode === 38 && cursor > 0) {
+    //   setCursor(prevState => prevState - 1)
+    // } else if (e.keyCode === 40 && cursor < results.length - 1) {
+    //   setCursor(prevState => prevState + 1)
+    // } else if (e.keyCode === 13 && cursor > -1 && results[cursor].itemRef) {
+    //   results[cursor].itemRef.focus()
+    //   results[cursor].itemRef.click()
+    // }
   }
+
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <FormControl
+      type="text"
+      onChange={handleChange}
+      placeholder="Search"
+      className="mr-sm-2 w-100"
+      value={searchQuery}
+      autoComplete="off"
+      autoCorrect="off"
+      spellCheck="false"
+      autoCapitalize="off"
+      ref={ref}
+      autoFocus={true}
+      onClick={e => {
+        e.preventDefault()
+      }}
+    />
+  ))
+
   return (
     <div ref={ref}>
       <Form inline className="ml-3 d-none d-md-inline-block">
         <Form.Group controlId="formSearch">
-          <FormControl
-            type="text"
-            onChange={handleChange}
-            placeholder="Search"
-            className="mr-sm-2 w-100"
-            value={searchQuery}
-            onBlur={e => setShow(false)}
-            onKeyDown={handleKeyDown}
-            autoComplete="off"
-            autoCorrect="off"
-            spellCheck="false"
-            autoCapitalize="off"
-          />
-          <Overlay
-            show={show}
-            target={target}
-            placement="bottom"
-            container={ref.current}
-            containerPadding={20}
-            className="test"
-          >
-            {results.length > 0 && (
-              <div
-                className="autocomplete-suggestions"
-                style={{ left: "1431px", top: "557px", width: "191px" }}
-                as="div"
-              >
-                {results.map((page, index) => (
-                  <Nav.Link
-                    key={page.title}
-                    className={`autocomplete-suggestion ${
-                      cursor > -1 && index === cursor ? "selected" : ""
-                    }`}
-                    href={page.url}
-                    tabIndex={index}
-                    ref={e => (page.itemRef = e)}
-                  >
-                    » {page.title}
-                  </Nav.Link>
-                ))}
-              </div>
-            )}
-          </Overlay>
+          <Dropdown className="w-100">
+            <Dropdown.Toggle
+              as={CustomToggle}
+              id="dropdown-custom-components"
+            ></Dropdown.Toggle>
+
+            <Dropdown.Menu
+              show={show}
+              ref={dropdownRef}
+              className={`w-100 autocomplete-suggestions ${show && "show"}`}
+            >
+              {results.map((page, index) => (
+                <Dropdown.Item
+                  active={cursor > -1 && index === cursor}
+                  eventKey={page.title}
+                  key={page.title}
+                  href={page.url}
+                  className="autocomplete-suggestion"
+                >
+                  » {page.title}
+                </Dropdown.Item>
+              ))}
+            </Dropdown.Menu>
+          </Dropdown>
         </Form.Group>
       </Form>
     </div>
