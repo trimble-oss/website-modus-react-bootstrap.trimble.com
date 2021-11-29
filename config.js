4b77789de5b5df70c0a4d5715a8006ed2e75a0cd
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2021-11-29 14:31:16 +0530

    PR comments

--- config.js
+++ config.js
@@ -1,14 +1,10 @@
 const reactBootstrapVersion =
   require("@trimbleinc/modus-react-bootstrap/package.json").version
 
-const bootstrapVersion =
-  require("@trimbleinc/modus-bootstrap/package.json").version
-
 const shortVersion = reactBootstrapVersion.split(".").slice(0, 2).join(".")
 
 const config = {
   reactBootstrapVersion,
-  bootstrapVersion,
   version: require("./package.json").version,
 }
 
