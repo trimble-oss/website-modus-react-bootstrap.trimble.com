535fba2b2794b62bb898b514659589987267f198
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2021-12-13 16:13:45 +0530

    DDS-316 Make Color Palette Consistent in React-Bootstrap

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const modusReactBootstrapVersion =
   require("@trimbleinc/modus-react-bootstrap/package.json").version
 
-const shortVersion = modusReactBootstrapVersion.split(".").slice(0, 2).join(".")
-
 const config = {
   modusReactBootstrapVersion,
   version: require("./package.json").version,
