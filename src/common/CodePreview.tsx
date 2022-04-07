5baf52d8ca4d96e467274c2341c4b44f5c956765
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-04-07 19:21:58 +0530

    Added an option to hide example code

--- src/common/CodePreview.tsx
+++ src/common/CodePreview.tsx
@@ -11,6 +11,13 @@ import React, {
 } from "react"
 import * as ReactBootstrap from "@trimbleinc/modus-react-bootstrap"
 import ReactDOM from "react-dom"
+import styled from "styled-components"
+
+const StyledAccordion = styled(ReactBootstrap.Accordion)`
+  .card-header::after {
+    display: none !important;
+  }
+`
 
 const CodePreview = props => {
   const scope = {
@@ -23,6 +30,11 @@ const CodePreview = props => {
     ...ReactBootstrap,
     ReactDOM,
     ...props.scope,
+  }
+
+  const [toggle, setToggle] = useState(props.hideCode)
+  const toggleCode = () => {
+    setToggle(prevState => !prevState)
   }
 
   return (
@@ -39,9 +51,32 @@ const CodePreview = props => {
         >
           <LivePreview className={props.className} />
         </Container>
-        <div className="border-top border-gray-200">
-          <LiveEditor />
-        </div>
+        <StyledAccordion
+          className="guide-code-options bg-light d-sm-flex align-items-center border-top border-gray-200"
+          defaultActiveKey={toggle ? null : "0"}
+        >
+          <ReactBootstrap.Card className="w-100">
+            <ReactBootstrap.Card.Header className="w-100 m-0 p-0">
+              <div class="d-flex justify-content-end w-100">
+                <div>
+                  <ReactBootstrap.Accordion.Toggle
+                    onClick={toggleCode}
+                    as={ReactBootstrap.Nav.Link}
+                    variant="link"
+                    eventKey="0"
+                  >
+                    {toggle ? "Show Code" : "Hide Code"}
+                  </ReactBootstrap.Accordion.Toggle>
+                </div>
+              </div>
+            </ReactBootstrap.Card.Header>
+            <ReactBootstrap.Accordion.Collapse eventKey="0">
+              <ReactBootstrap.Card.Body className="p-0">
+                <LiveEditor style={{ backgroundColor: "#fafafa" }} />
+              </ReactBootstrap.Card.Body>
+            </ReactBootstrap.Accordion.Collapse>
+          </ReactBootstrap.Card>
+        </StyledAccordion>
         <LiveError />
       </LiveProvider>
     </div>
