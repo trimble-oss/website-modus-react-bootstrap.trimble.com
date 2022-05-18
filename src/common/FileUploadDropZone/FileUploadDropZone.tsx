33345d20f1da05e48f62d7d2dc8ec2cbe7622d06
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-05-18 18:05:31 +0530

    save work

--- src/common/FileUploadDropZone/FileUploadDropZone.tsx
+++ src/common/FileUploadDropZone/FileUploadDropZone.tsx
@@ -1,4 +1,4 @@
-import { forwardRef } from "react"
+import { forwardRef, useCallback, useState } from "react"
 import classNames from "classnames"
 import * as PropTypes from "prop-types"
 import React from "react"
@@ -8,6 +8,7 @@ export interface FileUploadDropZoneProps
   maxFileCount?: number
   maxTotalFileSizeBytes?: number
   multiple?: boolean
+  onFiles?: (...args: any[]) => void
 }
 
 const propTypes = {
@@ -28,29 +29,129 @@ const propTypes = {
    *
    */
   multiple: PropTypes.bool,
+
+  /**
+   * Fires when files are being uploaded
+   */
+  onFiles: PropTypes.func,
 }
 
+const UPLOAD_INITIAL = "initial"
+const UPLOAD_DROP = "files drop"
+const UPLOAD_SUCCESS = "upload success"
+const UPLOAD_ERROR = "upload error"
 const FileUploadDropZone = forwardRef<HTMLDivElement, FileUploadDropZoneProps>(
   (
     {
       maxFileCount,
       maxTotalFileSizeBytes,
       multiple,
+      onFiles,
       className,
       ...props
     }: FileUploadDropZoneProps,
     ref
   ) => {
+    const [state, setState] = useState<{
+      status: string
+      background: string
+      text: string
+    }>({
+      status: UPLOAD_INITIAL,
+      background: "bg-white",
+      text: "text-secondary",
+    })
+    const handleDragEnter = useCallback(() => {
+      console.log("drag enter")
+      setState({
+        status: UPLOAD_DROP,
+        background: "bg-selected",
+        text: "text-primary",
+      })
+    }, [setState])
+    const handleDragLeave = useCallback(() => {
+      console.log("drag leave")
+      setState({
+        status: UPLOAD_INITIAL,
+        background: "bg-white",
+        text: "text-secondary",
+      })
+    }, [setState])
+    const handleDragOver = useCallback(e => e.preventDefault(), [])
+    const handleDrop = useCallback(
+      e => {
+        console.log("dropped")
+        e.preventDefault()
+        if (onFiles) onFiles(e)
+        setState({
+          status: UPLOAD_INITIAL,
+          background: "bg-white",
+          text: "text-secondary",
+        })
+      },
+      [onFiles, setState]
+    )
     return (
       <div
         {...props}
         ref={ref}
-        className={classNames("file-drop-zone", className)}
+        className={classNames(
+          "file-drop-zone",
+          state.background,
+          state.text,
+          className
+        )}
+        onDragEnter={handleDragEnter}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
       >
-        <div className="d-flex flex-column my-5 text-center text-primary">
-          <i className="modus-icons">cloud_upload</i>
-          Drag and Drop CSV here
-        </div>
+        {(() => {
+          switch (state.status) {
+            case UPLOAD_INITIAL:
+              return (
+                <div
+                  className={classNames(
+                    "d-flex flex-column my-5 text-center",
+                    state.text
+                  )}
+                >
+                  <i className="modus-icons">cloud_upload</i>
+                  <p>
+                    Drag files here or{" "}
+                    <span className="text-primary">browse</span> to upload.
+                  </p>
+                </div>
+              )
+            case UPLOAD_DROP:
+              return (
+                <div
+                  className={classNames(
+                    "d-flex flex-column my-5 text-center",
+                    state.text
+                  )}
+                >
+                  <i className="modus-icons">cloud_upload</i>
+                  <p>Drag files here.</p>
+                </div>
+              )
+            case UPLOAD_ERROR:
+              return (
+                <div
+                  className={classNames(
+                    "d-flex flex-column my-5 text-center",
+                    state.text
+                  )}
+                >
+                  <i className="modus-icons">no_entry</i>
+                  <p>Files exceed size limit.</p>
+                </div>
+              )
+
+            default:
+              return null
+          }
+        })()}
       </div>
     )
   }
---
+++ src/pages/sample/fileUploadDropZone.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Container, Row } from "@trimbleinc/modus-react-bootstrap"
+import { ModusIconsScripts } from "../../common/ExternalDependencyHelper"
+import "../../assets/css/main.scss"
+import FileUploadDropZone from "../../common/FileUploadDropZone/FileUploadDropZone"
+
+const FileUploadPage = props => {
+  return (
+    <main id="main">
+      <ModusIconsScripts />
+      <Container fluid className="pt-5">
+        <Container>
+          <Row>
+            <div className="col-12 col-lg-6 pt-5 mt-xl-5">
+              <div style={{ width: "400px" }}>
+                <h3 className="display-3">Upload Files</h3>
+                <h4>Max: 3 files (25 MB total)</h4>
+                <FileUploadDropZone id="test"></FileUploadDropZone>
+              </div>
+            </div>
+          </Row>
+        </Container>
+      </Container>
+    </main>
+  )
+}
+
+export default FileUploadPage
