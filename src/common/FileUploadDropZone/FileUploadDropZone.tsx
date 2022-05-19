519715194f264110016a360dcdc6c09551a6926c
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-05-19 15:12:13 +0530

    save work

--- src/common/FileUploadDropZone/FileUploadDropZone.tsx
+++ src/common/FileUploadDropZone/FileUploadDropZone.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useCallback, useState } from "react"
+import { forwardRef, useCallback, useRef, useState } from "react"
 import classNames from "classnames"
 import * as PropTypes from "prop-types"
 import React from "react"
@@ -8,7 +8,7 @@ export interface FileUploadDropZoneProps
   maxFileCount?: number
   maxTotalFileSizeBytes?: number
   multiple?: boolean
-  onFiles?: (...args: any[]) => void
+  onFiles?: (files: FileList, err: string) => void
 }
 
 const propTypes = {
@@ -36,10 +36,6 @@ const propTypes = {
   onFiles: PropTypes.func,
 }
 
-const UPLOAD_INITIAL = "initial"
-const UPLOAD_DROP = "files drop"
-const UPLOAD_SUCCESS = "upload success"
-const UPLOAD_ERROR = "upload error"
 const FileUploadDropZone = forwardRef<HTMLDivElement, FileUploadDropZoneProps>(
   (
     {
@@ -52,106 +48,110 @@ const FileUploadDropZone = forwardRef<HTMLDivElement, FileUploadDropZoneProps>(
     }: FileUploadDropZoneProps,
     ref
   ) => {
+    const resolvedRef = (useRef<HTMLDivElement>(null) ||
+      ref) as React.MutableRefObject<HTMLDivElement>
+    const dragCounter = useRef(0) // workaround for drag leave event firing on parent when dragging over child div
     const [state, setState] = useState<{
-      status: string
-      background: string
-      text: string
-    }>({
-      status: UPLOAD_INITIAL,
-      background: "bg-white",
-      text: "text-secondary",
-    })
-    const handleDragEnter = useCallback(() => {
-      console.log("drag enter")
-      setState({
-        status: UPLOAD_DROP,
-        background: "bg-selected",
-        text: "text-primary",
-      })
-    }, [setState])
-    const handleDragLeave = useCallback(() => {
-      console.log("drag leave")
-      setState({
-        status: UPLOAD_INITIAL,
-        background: "bg-white",
-        text: "text-secondary",
-      })
-    }, [setState])
-    const handleDragOver = useCallback(e => e.preventDefault(), [])
+      css?: string
+      icon?: React.ReactElement
+      message?: React.ReactElement | string
+    }>(null)
+
+    const handleDragEnter = useCallback(
+      e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        setState({
+          css: "files-dropping",
+          message: "Drag files here.",
+        })
+
+        dragCounter.current++
+      },
+      [setState]
+    )
+    const handleDragLeave = useCallback(
+      e => {
+        e.preventDefault()
+        e.stopPropagation()
+
+        dragCounter.current--
+        if (dragCounter.current === 0) {
+          setState(null)
+        }
+      },
+      [setState]
+    )
+    const handleDragOver = useCallback(e => {
+      e.preventDefault()
+      e.stopPropagation()
+    }, [])
     const handleDrop = useCallback(
       e => {
-        console.log("dropped")
         e.preventDefault()
-        if (onFiles) onFiles(e)
-        setState({
-          status: UPLOAD_INITIAL,
-          background: "bg-white",
-          text: "text-secondary",
-        })
+        e.stopPropagation()
+
+        let err = ValidateFiles(e.dataTransfer.files)
+        if (err) {
+          setState({
+            css: "files-invalid",
+            icon: <i className="modus-icons">no_entry</i>,
+            message: err,
+          })
+        } else setState(null)
+
+        if (onFiles) onFiles(e.dataTransfer.files, err)
       },
       [onFiles, setState]
     )
+
+    const ValidateFiles = useCallback(
+      (files: FileList) => {
+        if (files) {
+          let arr = Array.from(files)
+          // Total size
+          if (maxTotalFileSizeBytes) {
+            let totalSize = arr.reduce((tot, file) => {
+              return tot + file.size
+            }, 0)
+            if (totalSize > maxTotalFileSizeBytes)
+              return `Upload size exceeds limit. Max upload size ${maxTotalFileSizeBytes}bytes.`
+          }
+          // Multiple files upload
+          if (!multiple && arr.length > 1) {
+            return `Multiple files cannot be uploaded.`
+          }
+          if (multiple && maxFileCount && arr.length > maxFileCount) {
+            return `Max files can be uploaded exceeds limit. Number of files uploaded is ${arr.length} and the limit is ${maxFileCount}.`
+          }
+        }
+        return null
+      },
+      [maxFileCount, maxTotalFileSizeBytes, multiple]
+    )
+
     return (
       <div
         {...props}
-        ref={ref}
-        className={classNames(
-          "file-drop-zone",
-          state.background,
-          state.text,
-          className
-        )}
+        ref={resolvedRef}
+        className={classNames("file-drop-zone", state && state.css, className)}
         onDragEnter={handleDragEnter}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
       >
-        {(() => {
-          switch (state.status) {
-            case UPLOAD_INITIAL:
-              return (
-                <div
-                  className={classNames(
-                    "d-flex flex-column my-5 text-center",
-                    state.text
-                  )}
-                >
-                  <i className="modus-icons">cloud_upload</i>
-                  <p>
-                    Drag files here or{" "}
-                    <span className="text-primary">browse</span> to upload.
-                  </p>
-                </div>
-              )
-            case UPLOAD_DROP:
-              return (
-                <div
-                  className={classNames(
-                    "d-flex flex-column my-5 text-center",
-                    state.text
-                  )}
-                >
-                  <i className="modus-icons">cloud_upload</i>
-                  <p>Drag files here.</p>
-                </div>
-              )
-            case UPLOAD_ERROR:
-              return (
-                <div
-                  className={classNames(
-                    "d-flex flex-column my-5 text-center",
-                    state.text
-                  )}
-                >
-                  <i className="modus-icons">no_entry</i>
-                  <p>Files exceed size limit.</p>
-                </div>
-              )
-
-            default:
-              return null
-          }
-        })()}
+        <div className={classNames("d-flex flex-column my-5 text-center")}>
+          {(state && state.icon) || <i className="modus-icons">cloud_upload</i>}
+          <p>
+            {(state && state.message) || (
+              <>
+                Drag files here or
+                <span className="text-primary">browse</span> to upload.
+              </>
+            )}
+          </p>
+        </div>
       </div>
     )
   }
