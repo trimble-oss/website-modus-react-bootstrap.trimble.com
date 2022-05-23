d572c78abb5e1159e61086848398cc29c60ec145
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-05-23 18:35:41 +0530

    Fix accesslint bugs

--- src/common/FileUploadDropZone/FileUploadDropZoneStyled.tsx
+++ src/common/FileUploadDropZone/FileUploadDropZoneStyled.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import * as React from "react"
-import { HTMLAttributes } from "react"
-import styled, { css } from "styled-components"
+import styled from "styled-components"
 
 export const StyledDivWrapper = React.forwardRef<
   HTMLDivElement,
