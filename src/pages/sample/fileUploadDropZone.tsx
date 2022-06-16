b5b1df49fa0c9b0c7dec5d2da92c6d42a8b55b95
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-06-16 20:54:50 +0530

    fix build errors

--- src/pages/sample/fileUploadDropZone.tsx
+++ src/pages/sample/fileUploadDropZone.tsx
@@ -1,56 +1,61 @@
-// import * as React from "react"
-// import { Container, Row } from "@trimbleinc/modus-react-bootstrap"
-// import { ModusIconsScripts } from "../../common/ExternalDependencyHelper"
-// import "../../assets/css/main.scss"
-// import { FileUploadDropZone } from "../../common/modus-react-bootstrap"
+import * as React from "react"
+import {
+  Container,
+  Row,
+  FileUploadDropZone,
+} from "@trimbleinc/modus-react-bootstrap"
+import { ModusIconsScripts } from "../../common/ExternalDependencyHelper"
+import "../../assets/css/main.scss"
 
-// const Example = () => {
-//   const [filesUploaded, setFilesUploaded] = React.useState<FileList>(null)
+const Example = () => {
+  const [filesUploaded, setFilesUploaded] = React.useState<FileList | null>(
+    null
+  )
 
-//   return (
-//     <div>
-//       <h3 className="display-3">Upload Files</h3>
-//       <h4>Max: 3 files (25 MB total)</h4>
-//       <FileUploadDropZone
-//         id="test"
-//         maxFileCount={2}
-//         maxTotalFileSizeBytes={5000}
-//         onFiles={(files, err) => {
-//           if (!err) setFilesUploaded(files)
-//         }}
-//         style={{ width: "400px", height: "200px" }}
-//         aria-label="Drop Zone"
-//       ></FileUploadDropZone>
+  return (
+    <div>
+      <h3 className="display-3">Upload Files</h3>
+      <h4>Max: 3 files (25 MB total)</h4>
+      <FileUploadDropZone
+        id="test"
+        maxFileCount={2}
+        maxTotalFileSizeBytes={5000}
+        onFiles={(files, err) => {
+          if (!err) setFilesUploaded(files)
+        }}
+        style={{ width: "400px", height: "200px" }}
+        aria-label="Drop Zone"
+      ></FileUploadDropZone>
 
-//       {filesUploaded && (
-//         <>
-//           <h4 className="mt-5">Files uploaded:</h4>
-//           <ul className="list-group list-group-borderless">
-//             {Array.from(filesUploaded).map(file => (
-//               <li className="list-group-item list-item-left-control">
-//                 <i className="modus-icons">check_circle</i>
-//                 <span>{file.name}</span>
-//               </li>
-//             ))}
-//           </ul>
-//         </>
-//       )}
-//     </div>
-//   )
-// }
-// const FileUploadPage = props => {
-//   return (
-//     <main id="main">
-//       <ModusIconsScripts />
-//       <Container fluid className="pt-5">
-//         <Container>
-//           <Row>
-//             <Example />
-//           </Row>
-//         </Container>
-//       </Container>
-//     </main>
-//   )
-// }
+      {filesUploaded && (
+        <>
+          <h4 className="mt-5">Files uploaded:</h4>
+          <ul className="list-group list-group-borderless">
+            {Array.from(filesUploaded).map(file => (
+              <li className="list-group-item list-item-left-control">
+                <i className="modus-icons">check_circle</i>
+                <span>{file.name}</span>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  )
+}
+const FileUploadPage = props => {
+  return (
+    <main id="main">
+      <ModusIconsScripts />
+      <Container fluid className="pt-5">
+        <Container>
+          <Row>
+            <Example />
+          </Row>
+        </Container>
+      </Container>
+    </main>
+  )
+}
 
-// export default FileUploadPage
+export default FileUploadPage
--- src/pages/sample/react-table.tsx
+++ src/pages/sample/react-table.tsx
@@ -1,92 +1,90 @@
-// import React, { useState, useMemo, useCallback } from "react"
-// import styled from "styled-components"
-// import {
-//   Button,
-//   Chip,
-//   Col,
-//   Container,
-//   Form,
-//   Nav,
-//   OverlayTrigger,
-//   Popover,
-//   Row,
-// } from "@trimbleinc/modus-react-bootstrap"
-// import { ModusIconsScripts } from "../../common/ExternalDependencyHelper"
-// import {
-//   DataTable,
-//   Table,
-//   TablePagination,
-// } from "../../common/modus-react-bootstrap"
-// import { MakeData as makeData } from "../../examples/components/Table"
-// import LinkedHeading from "../../common/LinkedHeading"
-// import { bool, string } from "prop-types"
+import React, { useState, useMemo, useCallback } from "react"
+import styled from "styled-components"
+import {
+  Button,
+  Chip,
+  Col,
+  Container,
+  Form,
+  Nav,
+  OverlayTrigger,
+  Popover,
+  Row,
+  DataTable,
+  Table,
+  TablePagination,
+} from "@trimbleinc/modus-react-bootstrap"
+import { ModusIconsScripts } from "../../common/ExternalDependencyHelper"
+import { MakeData as makeData } from "../../examples/components/Table"
+import LinkedHeading from "../../common/LinkedHeading"
+import { bool, string } from "prop-types"
 
-// function Example() {
-//   const columns = React.useMemo(
-//     () => [
-//       {
-//         Header: "First Name",
-//         accessor: "firstName",
-//         sortBy: true,
-//       },
-//       {
-//         Header: "Last Name",
-//         accessor: "lastName",
-//         sortBy: true,
-//       },
-//       {
-//         Header: "Age",
-//         accessor: "age",
-//         sortBy: true,
-//       },
-//       {
-//         Header: "Visits",
-//         accessor: "visits",
-//         sortBy: true,
-//       },
-//       {
-//         Header: "Status",
-//         accessor: "status",
-//         sortBy: true,
-//       },
-//       {
-//         Header: "Profile Progress Status",
-//         accessor: "progress",
-//         sortBy: true,
-//       },
-//     ],
-//     []
-//   )
+function Example() {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "First Name",
+        accessor: "firstName",
+        sortBy: true,
+      },
+      {
+        Header: "Last Name",
+        accessor: "lastName",
+        sortBy: true,
+      },
+      {
+        Header: "Age",
+        accessor: "age",
+        sortBy: true,
+      },
+      {
+        Header: "Visits",
+        accessor: "visits",
+        sortBy: true,
+      },
+      {
+        Header: "Status",
+        accessor: "status",
+        sortBy: true,
+      },
+      {
+        Header: "Profile Progress Status",
+        accessor: "progress",
+        sortBy: true,
+      },
+    ],
+    []
+  )
 
-//   // makeData: a custom helper function to generate random rows
-//   // for the demo purpose not implemented here in the example.
-//   const data = React.useMemo(() => makeData(30), [])
+  // makeData: a custom helper function to generate random rows
+  // for the demo purpose not implemented here in the example.
+  const data = React.useMemo(() => makeData(30), [])
 
-//   return (
-//     <DataTable
-//       id="dt_sorting"
-//       columns={columns}
-//       bordered
-//       hover
-//       pageSize={7}
-//       pageSizeOptions={[7, 10, 25, 50]}
-//       data={data}
-//     ></DataTable>
-//   )
-// }
-// const ReactTablePage = props => {
-//   return (
-//     <main id="main">
-//       <ModusIconsScripts />
-//       <Container fluid className="pt-5">
-//         <Container>
-//           <Row>
-//             <Example />
-//           </Row>
-//         </Container>
-//       </Container>
-//     </main>
-//   )
-// }
+  return (
+    <DataTable
+      id="dt_sorting"
+      columns={columns}
+      bordered
+      hover
+      pageSize={7}
+      pageSizeOptions={[7, 10, 25, 50]}
+      data={data}
+    ></DataTable>
+  )
+}
+const ReactTablePage = props => {
+  return (
+    <main id="main">
+      <ModusIconsScripts />
+      <Container fluid className="pt-5">
+        <Container>
+          <Row>
+            <Example />
+          </Row>
+        </Container>
+      </Container>
+    </main>
+  )
+}
 
-// export default ReactTablePage
+export default ReactTablePage
