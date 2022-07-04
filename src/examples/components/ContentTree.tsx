1878595a0172a33e71c4c9aebdb137bdb985610e
Mahalakshmi Sankaran <mahalakshmi_sankaran@trimble.com>
2022-07-04 17:06:44 +0530

    Test content tree with indicator
    Also fixed DataTable with checkbox column width issue
    
    And updated the components from modus-react-bootstrap for API generation

--- src/examples/components/ContentTree.tsx
+++ src/examples/components/ContentTree.tsx
@@ -1218,3 +1218,110 @@ export const StyledCustomTreeViewItem = styled.div`
     }
   }
 `
+export const StyledTreeItem = styled.div`
+  position: relative;
+  &.expanded:before {
+    content: "";
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 0.3rem;
+    height: 100%;
+    z-index: 5;
+    -webkit-box-shadow: inset 0.3rem 0 0 0 #217cbb;
+    -moz-box-shadow: inset 0.3rem 0 0 0 #217cbb;
+    box-shadow: inset 0.3rem 0 0 0 #217cbb;
+  }
+`
+
+export const TreeViewWithIndicator = `
+const TreeViewContext = createContext(null);
+
+const CustomTreeViewItem = ({ nodeId, label, children, ...props }) => {
+  const expanded = useContext(TreeViewContext);
+
+  /* StyledTreeItem
+  {
+    position: relative;
+    &.expanded:before {
+      content: '';
+      position: absolute;
+      left: 0;
+      right: 0;
+      width: 0.3rem;
+      height: 100%;
+      z-index: 5;
+      -webkit-box-shadow: inset 0.3rem 0 0 0 #217cbb;
+      -moz-box-shadow: inset 0.3rem 0 0 0 #217cbb;
+      box-shadow: inset 0.3rem 0 0 0 #217cbb;
+    }
+  }
+  */
+
+  return (
+    <StyledTreeItem
+      className={expanded === nodeId ? 'expanded' : undefined}
+    >
+      <TreeViewItem nodeId={nodeId} label={label} {...props}>
+        {children}
+      </TreeViewItem>
+    </StyledTreeItem>
+  );
+};
+
+function TreeViewWithIndicator() {
+  const [expanded, setExpanded] = useState(null);
+
+  const handleNodeToggle = useCallback(
+    (event, nodes, nodeInFocus) => {
+      if (nodes.includes(nodeInFocus)) setExpanded(nodeInFocus);
+      else {
+        const prevExpandedNode = expanded;
+        setExpanded(
+          prevExpandedNode && nodes.includes(prevExpandedNode)
+            ? prevExpandedNode
+            : null,
+        );
+      }
+    },
+    [expanded, setExpanded],
+  );
+
+  return (
+    <div style={{ width: '400px' }} >
+      <TreeViewContext.Provider value={expanded}>
+        <TreeView nodeId={0} id="basic" onNodeToggle={handleNodeToggle}>
+          <div>
+            <CustomTreeViewItem nodeId={7} label="Inbox">
+              <TreeViewItem nodeId={8} label="Personal"></TreeViewItem>
+              <TreeViewItem nodeId={9} label="Work"></TreeViewItem>
+              <TreeViewItem nodeId={10} label="Community"></TreeViewItem>
+              <TreeViewItem nodeId={11} label="Social"></TreeViewItem>
+              <TreeViewItem nodeId={12} label="Friends"></TreeViewItem>
+              <TreeViewItem nodeId={14} label="More ..."></TreeViewItem>
+            </CustomTreeViewItem>
+          </div>
+
+          <CustomTreeViewItem nodeId={15} label="Sent">
+            <TreeViewItem nodeId={16} label="Mail1"></TreeViewItem>
+            <TreeViewItem nodeId={17} label="Mail2"></TreeViewItem>
+            <TreeViewItem nodeId={18} label="Mail3"></TreeViewItem>
+          </CustomTreeViewItem>
+          <CustomTreeViewItem nodeId={1} label="Archived">
+            <CustomTreeViewItem nodeId={2} label="Folder1">
+              <TreeViewItem nodeId={3} label="File1"></TreeViewItem>
+              <CustomTreeViewItem nodeId={4} label="Folder2">
+                <TreeViewItem nodeId={5} label="File2"></TreeViewItem>
+              </CustomTreeViewItem>
+              <TreeViewItem nodeId={6} label="File3"></TreeViewItem>
+            </CustomTreeViewItem>
+          </CustomTreeViewItem>
+          <TreeViewItem nodeId={19} label="Spam"></TreeViewItem>
+        </TreeView>
+      </TreeViewContext.Provider>
+    </div>
+  );
+}
+
+render(<TreeViewWithIndicator />);
+`
